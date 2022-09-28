d8b6aa83e0d03e760a79a86920e02a70578a1e2b
Valandy <andinachvalentin@gmail.com>
2022-09-28 17:46:53 -0300

    re_paths, redirects y recolección de atributos GET

--- cac/urls.py
+++ cac/urls.py
@@ -1,8 +1,24 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
+    path('',views.index, name='inicio'),
     path('hola_mundo', views.hola_mundo),
-    path('saludar/<str:nombre>', views.saludar),
+    
+    #saludos
+    path('saludar/',views.saludar, name='saludar_solito'),
+    path('saludar/<str:nombre>', views.saludar, name='saludar'),
+    
+    
+    #about
+    path('quienessomos/',views.quienes_somos, name='quienes_somos'),
+    
+    #proyectos
+    path('proyectos/<int:anio>/<int:mes>',views.ver_proyectos,name="ver_proyectos"),
+    re_path(r'^proyectos/(?P<anio>\d{4})/$',views.ver_proyectos,name="ver_proyectos_anio"),
+    
+    #cursos
+    path('cursos/detalle/<slug:nombre_curso>/',views.cursos_detalle,name='cursos_detalle'),
+    re_path(r'^cursos/(?P<nombre_categoria>\w+)/$',views.cursos,name='cursos'),
 ]
--- cac/views.py
+++ cac/views.py
@@ -1,13 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from django.urls import reverse
 
 
 # Create your views here.
+
+def index(request):
+    if(request.method=='GET'):
+        titulo = 'Título cuando accedo por GET'
+    else:
+        titulo = f'Título cuando accedo por otr método {request.method}'
+    parametros_get = request.GET.get('nombre')
+    return HttpResponse(f"""
+        <h1>{titulo}</h1>
+        <p>{parametros_get}</p>
+        """)
+
 def hola_mundo(request):
     return HttpResponse('Hola mundo Django')
     pass
 
-def saludar(request,nombre):
+def saludar(request,nombre='Valentín'):
     return HttpResponse(f"""
         <h1>Hola mundo Django - un gusto {nombre}</h1>    
     """)
+    
+def ver_proyectos(request,anio,mes=1):
+    return HttpResponse(f"""
+        <h1>Proyectos del {mes}/{anio}</h1>
+        <p>Listado de proyectos</p>
+        """)
+    
+def cursos_detalle(request, nombre_curso):
+    return HttpResponse(f'{nombre_curso}')
+
+    
+def cursos(request, nombre_categoria):
+    return HttpResponse(f'{nombre_categoria}')
+
+def quienes_somos(request):
+    return redirect(reverse('saludar', kwargs={'nombre' : 'Roberto'}))
+#   return redirect('saludar_solito')
